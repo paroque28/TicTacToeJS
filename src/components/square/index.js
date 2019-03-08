22e8a1bd2a6b18698391663c81b74442b0bdf57b
Pablo Rodriguez <paroque28@gmail.com>
2019-03-07 18:30:41 -0600

    Final

--- src/components/square/index.js
+++ src/components/square/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-export default class Square extends React.Component {
-  //Aqui es JS
-  render() {
-    return <button className="square">{/* TODO: AQUI es JSX*/}</button>;
-  }
-}
+const Square = props => {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+};
+
+export default Square;
